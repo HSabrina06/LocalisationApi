f4586ea45a2a2b8e15b007566f0fb426b206e667
Sabrina <anabila>
2022-06-16 10:52:00 +0300

    remove not used import

--- src/main/java/com/api/localisation/Dao/LocalisationDao.java
+++ src/main/java/com/api/localisation/Dao/LocalisationDao.java
@@ -6,12 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Page;
+
 import org.springframework.data.domain.Pageable;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface LocalisationDao  extends JpaRepository<Localisation, Long> {
